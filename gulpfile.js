292531b49a2cdcf9c3d15a6e45b40fcde0cd9460
Heneros <punk60826@gmail.com>
2022-02-20 20:11:06 +0200

    email field in footer

--- gulpfile.js
+++ gulpfile.js
@@ -36,15 +36,11 @@ const cssFiles = [
   "src/sass/style.scss",
   "node_modules/slick-slider/slick/slick.css",
   "node_modules/slick-slider/slick/slick-theme.css",
-  "node_modules/swiper/swiper.min.css",
-
 ];
 
 const scripts = [ 
   "node_modules/jquery/dist/jquery.min.js",
   "node_modules/slick-slider/slick/slick.min.js",
-  "node_modules/swiper/swiper-bundle.min.js",
-  
   "src/js/*.js",
 ];
 
